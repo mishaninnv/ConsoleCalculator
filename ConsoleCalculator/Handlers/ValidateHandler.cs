62fd42fd69bb88cd964838bf7ec295be47c41ec5
Nik Mish <mishaninnv93@gmail.com>
2023-02-01 14:46:50 +0500

    Add ValidateHandler

---
+++ ConsoleCalculator/Handlers/ValidateHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace ConsoleCalculator.Handlers;
+
+public class ValidateHandler
+{
+    private readonly string? _divisionByZeroPattern = ConfigurationManager.AppSettings["DivisionByZeroPattern"];
+    private readonly string? _validSymbols = ConfigurationManager.AppSettings["ValidSymbols"];
+
+    public bool CheckValidExpression(ExpressionModel model)
+    {
+        if (CheckDivisionByZero(model.Expression))
+        {
+            model.Messages.Add("Присутствует деление на ноль");
+        }
+        if (!CheckSymbols(model.Expression))
+        {
+            model.Messages.Add("Присутствуют недопустимые символы");
+        }
+        if (!CheckParenthesesSeries(model.Expression))
+        {
+            model.Messages.Add("Скобки расставлены неправильно");
+        }
+        return model.Messages.Count == 0;
+    }
+
+    private bool CheckDivisionByZero(string expr) => Regex.Match(expr, _divisionByZeroPattern).Success;
+
+    private bool CheckSymbols(string expr) => Regex.Match(expr, _validSymbols + $"{{ { expr.Length} }}").Success;
+
+    private bool CheckParenthesesSeries(string expr)
+    {
+        var counter = 0;
+
+        foreach (var symbol in expr)
+        {
+            if (symbol.Equals('(')) counter++;
+            else if (symbol.Equals(')')) counter--;
+
+            if (counter < 0) return false;
+        }
+
+        return counter == 0;
+    }
+}
